f387b5f56da02cbc0775cfdcccffe31ee520d9f8
MohamedBadra <MohamedWBadra@gmail.com>
2026-03-09 01:19:49 +0200

    db small update

--- Cellula_5week_[MohamedBadra]/app/rag/vector_db.py
+++ Cellula_5week_[MohamedBadra]/app/rag/vector_db.py
@@ -11,9 +11,7 @@ from langchain_core.chat_history import BaseChatMessageHistory
 from operator import itemgetter
 
 github_token="github_key"
-# 1. Setup Models
 embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-# llm = ChatOpenAI(model_name="gpt-4o-mini", openai_api_key=a, temperature=1)
 llm = ChatOpenAI(
     model="meta/Llama-4-Scout-17B-16E-Instruct",
     api_key=github_token,
@@ -23,8 +21,6 @@ llm = ChatOpenAI(
     model_kwargs={"top_p": 0.1}
 )
 
-# 2. Setup Database
-# Using your exact local path logic
 current_dir = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(current_dir, "../../Code_generator_chromaDB copy")
 vectorstore = Chroma(
@@ -34,7 +30,6 @@ vectorstore = Chroma(
 )
 retriever = vectorstore.as_retriever(search_kwargs={"k": 2})
 
-# 3. Setup RAG Chain
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are an expert Python coding assistant. Answer using ONLY context. Format with Markdown. If not in context, say 'I don't know'.\n\nContext:\n{context}"),
     MessagesPlaceholder(variable_name="chat_history"),
